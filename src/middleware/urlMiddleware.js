e14bf4a39d49dc137f96fdeab741a9cd303bcea8
Miguel Domingos <109294009+domingosmiguel@users.noreply.github.com>
2022-12-22 21:23:31 -0300

    fix: urlMiddleware passing wrong object

--- src/middleware/urlMiddleware.js
+++ src/middleware/urlMiddleware.js
@@ -1,8 +1,8 @@
 import urlSchema from '../schemas/urlSchema.js';
 
 export default function urlMiddleware(req, res, next) {
-  const url = req.body;
-  const validation = urlSchema.validate(url, { abortEarly: false });
+  const { url } = req.body;
+  const validation = urlSchema.validate({ url }, { abortEarly: false });
   if (validation.error) {
     const errors = validation.error.details.map((detail) => detail.message);
     return res.status(422).send(errors);
